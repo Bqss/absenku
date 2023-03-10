c95a39b2af5f23e6a52fac14c844deab01f5aae5
Bqss <basofi.cucokmeong12@gmail.com>
2023-03-10 14:40:00 +0700

    implement mobile scan

--- application/views/scan/scan_mobile.php
+++ application/views/scan/scan_mobile.php
@@ -16,7 +16,7 @@
       <div class='box'>
         <div class='box-header'></div>
         <div class='box-body'>
-          <?php $attributes = array('id' => 'button');
+          <?php $attributes = array('id' => 'QR');
           echo form_open('scan/cek_id', $attributes); ?>
           <div id="sourceSelectPanel" style="display:'none';">
             <label for="sourceSelect">Change video source:</label>
@@ -35,7 +35,7 @@
             <video id="video" width="100%" style="border: 1px solid gray"></video>
           </div>
 
-          <textarea name="nis" hidden id="result" readonly></textarea>
+          <input type="text" name="no_induk" hidden id="result" >
           <span> <input type="submit" id="button" class="btn btn-success btn-md" value="Cek Kehadiran"></span>
           <?php echo form_close(); ?>
         </div>
@@ -43,7 +43,10 @@
     </div>
   </div>
 </section>
-
+<script type="text/javascript" src="<?php echo base_url() ?>assets/plugins/sweetalert/sweetAlert.min.js"></script>
+<script>
+  <?= $this->session->flashdata('messageAlert'); ?>
+</script>
 <script type="text/javascript" src="<?php echo base_url() ?>assets/plugins/zxing/zxing.min.js"></script>
 <script type="text/javascript">
   // mengambil data kamera , yaitu arah kamera ke belakang/ atau depan  
@@ -54,17 +57,17 @@
     let selectedDeviceId;
     let audio = new Audio("assets/audio/beep.mp3");
     const codeReader = new ZXing.BrowserQRCodeReader()
-    console.log('ZXing code reader initialized')
+
 
     //
     const decode = () => {
       codeReader.decodeFromInputVideoDevice(selectedDeviceId, 'video').
       then((result) => {
-        console.log(result)
-        document.getElementById('result').textContent = result.text
+ 
+        document.getElementById('result').value = result.text
         if (result != null) {
           audio.play();
-          // $('#button').submit();
+          $('#QR').submit();
         }
 
       }).catch((err) => {
