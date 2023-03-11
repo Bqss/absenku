e1cfa513e6b3c0c0fd38b0d84ab89eed6c6edc1e
Bqss <basofi.cucokmeong12@gmail.com>
2023-03-11 07:25:20 +0700

    fixing bug

--- application/views/template/datatables.php
+++ application/views/template/datatables.php
@@ -1,17 +1,17 @@
-<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/plugins/DataTables/1.10.18/css/dataTables.bootstrap.min.css" />
-<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/plugins/DataTables/buttons/1.5.6/css/buttons.bootstrap.min.css" />
-<script type="text/javascript" src="<?php echo base_url() ?>assets/plugins/DataTables/jszip/2.5.0/jszip.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url() ?>assets/plugins/DataTables/pdfmake/0.1.36/pdfmake.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url() ?>assets/plugins/DataTables/pdfmake/0.1.36/vfs_fonts.js"></script>
-<script type="text/javascript" src="<?php echo base_url() ?>assets/plugins/DataTables/1.10.18/js/jquery.dataTables.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url() ?>assets/plugins/DataTables/1.10.18/js/dataTables.bootstrap.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url() ?>assets/plugins/DataTables/buttons/1.5.6/js/dataTables.buttons.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url() ?>assets/plugins/DataTables/buttons/1.5.6/js/buttons.bootstrap.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url() ?>assets/plugins/DataTables/buttons/1.5.6/js/buttons.colVis.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url() ?>assets/plugins/DataTables/buttons/1.5.6/js/buttons.flash.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url() ?>assets/plugins/DataTables/buttons/1.5.6/js/buttons.html5.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url() ?>assets/plugins/DataTables/buttons/1.5.6/js/buttons.print.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url() ?>assets/plugins/DataTables/progress/percentageBars.js"></script>
+<link rel="stylesheet"  href="<?php echo base_url() ?>assets/plugins/DataTables/1.10.18/css/dataTables.bootstrap.min.css" />
+<link rel="stylesheet"  href="<?php echo base_url() ?>assets/plugins/DataTables/buttons/1.5.6/css/buttons.bootstrap.min.css" />
+<script  src="<?php echo base_url() ?>assets/plugins/DataTables/jszip/2.5.0/jszip.min.js"></script>
+<script  src="<?php echo base_url() ?>assets/plugins/DataTables/pdfmake/0.1.36/pdfmake.min.js"></script>
+<script  src="<?php echo base_url() ?>assets/plugins/DataTables/pdfmake/0.1.36/vfs_fonts.js"></script>
+<script  src="<?php echo base_url() ?>assets/plugins/DataTables/1.10.18/js/jquery.dataTables.min.js"></script>
+<script  src="<?php echo base_url() ?>assets/plugins/DataTables/1.10.18/js/dataTables.bootstrap.min.js"></script>
+<script  src="<?php echo base_url() ?>assets/plugins/DataTables/buttons/1.5.6/js/dataTables.buttons.min.js"></script>
+<script  src="<?php echo base_url() ?>assets/plugins/DataTables/buttons/1.5.6/js/buttons.bootstrap.min.js"></script>
+<script  src="<?php echo base_url() ?>assets/plugins/DataTables/buttons/1.5.6/js/buttons.colVis.min.js"></script>
+<script  src="<?php echo base_url() ?>assets/plugins/DataTables/buttons/1.5.6/js/buttons.flash.min.js"></script>
+<script  src="<?php echo base_url() ?>assets/plugins/DataTables/buttons/1.5.6/js/buttons.html5.min.js"></script>
+<script  src="<?php echo base_url() ?>assets/plugins/DataTables/buttons/1.5.6/js/buttons.print.min.js"></script>
+<script  src="<?php echo base_url() ?>assets/plugins/DataTables/progress/percentageBars.js"></script>
 <script src="<?php echo base_url() ?>assets/plugins/DataTables/FilterDropdown/filterDropDown.min.js"></script>
 <script src="<?php echo base_url() ?>assets/app/core/dtconfig.js"></script>
 
