70fcf65629d45e0fde4f82c8db2e0f5a78f2d2d3
aufall02 <aufaluzumaki@gmail.com>
2023-06-10 00:06:04 +0700

    update

--- application/models/AyamJago_model.php
+++ application/models/AyamJago_model.php
@@ -1,16 +1,31 @@
 <?php
 class AyamJago_model extends CI_Model
 {
-    public $table = 'barcode';
-    public $id = 'id_barcode';
+    public $table = 'ayam_jago';
+    public $id = 'id_ayam_jago';
     public $order = 'DESC';
-    public $id_karyawan = 'id_karyawan';
     public function __construct()
     {
         parent::__construct();
     }
-    public function index()
+    function get_all()
+  {
+    $this->db->order_by($this->id, $this->order);
+    return $this->db->get($this->table)->result();
+  }
+
+    function get_by_no_induk($id)
     {
-        echo "Ayam Jago";
+        $sql = "SELECT * from ayam_jago where nis=$id ";
+        return $this->db->query($sql)->row($id);
     }
+
+    function check_ayam_jago(){
+    $this->db->select('aj.id_ayam_jago, t2.something,')
+     ->from('ayam_jago as aj')
+     ->where('t1.id', $id)
+     ->join('table2 as t2', 't1.id = t2.id', 'LEFT')
+     ->get();
+    }
+
 }
